c5ec7a7f5b41c34163906b054be3e330a3612c85
mgmzdbh25365 <98370342+mgmzdbh25365@users.noreply.github.com>
2023-08-19 15:50:31 +0800

    Add files via upload

---
+++ Details_Patch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace SuperMinerMod
+{
+	[HarmonyPatch(typeof(AdditionalDetailsPanel) , "RefreshDetails")]
+	public class Details_Patch
+	{
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+		{
+			List<CodeInstruction> code = instr.ToList();
+			foreach (CodeInstruction codeInstruction in code)
+			{
+				if (codeInstruction.opcode == OpCodes.Ldstr && (string)codeInstruction.operand == "0.000")
+					codeInstruction.operand = "0.000################################################################################################################################################################################################################################################################################################################################################";
+				yield return codeInstruction;
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ONI-Truthful_Thermal_Conductivity
-It's just a mod for "OxygenNotIncluded".
+
+![](https://github.com/mgmzdbh25365/ONI-Truthful_Thermal_Conductivity/ONI-Truthful Thermal Conductivity-cover.png)
+### Features
+
+- Thermal conductivity is displayed without rounding, including properties, database, tooltips, info cards(if any), etc.
+
+![](https://github.com/mgmzdbh25365/ONI-Truthful_Thermal_Conductivity/ONI-Truthful Thermal Conductivity.png)
+[source](https://github.com/mgmzdbh25365/ONI-Truthful_Thermal_Conductivity)
+
+---------------------------------------------
+# 缺氧-精确的热导率
+
+### 主要特性
+
+- 让热导率正常显示，包括属性，数据库，工具栏，信息栏等，拒绝四舍五入。
+
+![](https://github.com/mgmzdbh25365/ONI-Truthful_Thermal_Conductivity/ONI-Truthful Thermal Conductivity-zh.png)
+[源码](https://github.com/mgmzdbh25365/ONI-Truthful_Thermal_Conductivity)
